dbfa205c400bd3854af097363f6ce160a992f9df
g-div <g-div@anche.no>
2014-12-08 16:30:48 +0100

    add new fields according to the data found

--- api/models/Material.js
+++ api/models/Material.js
@@ -5,6 +5,8 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
+var RATINGS = [1, 2, 3, 4, 5];
+
 module.exports = {
   descriptions: {
     name: "The name of the material",
@@ -16,13 +18,32 @@ module.exports = {
   		notEmpty: true,
   		unique: true,
   		required: true
-  	},
+    },
+    ratingEnv: {
+      type: 'integer',
+      enum: RATINGS,
+      int: true
+    },
+    ratingSoc: {
+      type: 'integer',
+      enum: RATINGS,
+      int: true
+    },
+    ratingHum: {
+      type: 'integer',
+      enum: RATINGS,
+      int: true
+    },
+    minedIn: {
+      type: 'array',
+      array: true
+    },
     weight: {
       type: "integer",
       int: true
     },
     inPart: {
-    	model: "part"
+      model: "part"
     }
   }
 };
