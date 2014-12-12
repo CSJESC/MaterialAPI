47156f54fc45b4207d81b3d01a44827b2838c903
g-div <g-div@anche.no>
2014-12-12 16:03:05 +0100

    fix land rating

--- api/models/Land.js
+++ api/models/Land.js
@@ -4,6 +4,7 @@
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
+var RATINGS = [1, 2, 3, 4, 5];
 
 module.exports = {
   descriptions: {
