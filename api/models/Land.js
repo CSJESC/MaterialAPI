f1b6f1c2bd43a2a41cb61ba2bea97e5cbde22643
g-div <g-div@anche.no>
2015-01-11 09:19:51 +0100

    land and materials entrypoints updated

--- api/models/Land.js
+++ api/models/Land.js
@@ -4,12 +4,14 @@
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
-var RATINGS = [1, 2, 3, 4, 5];
 
 module.exports = {
   descriptions: {
-    name: "The land's name",
-    ragint: "The rating fo this country"
+    name: "The country's name",
+    mineralIndustryRating: "The Mineral Industry rating fo this country",
+    workingConditionRating: "The rating about the Rating Codintions in this country",
+    humanRightsRating: "The rating about Human Rights in this country",
+    localAvarageSalary: "The local avarage salary in this country"
   },
   attributes: {
     name: {
@@ -19,15 +21,43 @@ module.exports = {
       unique: true,
       required: true
     },
-    rating: {
+    mineralIndustryRating: {
       type: "integer",
-      enum: RATINGS,
+      int: true,
+      min: 0,
+      max: 70
+    },
+    workingConditionsRating: {
+      type: "integer",
+      int: true,
+      min: 1,
+      max: 5
+    },
+    humanRightsRating: {
+      type: "integer",
+      int: true,
+      min: 0,
+      max: 100
+    },
+    localAvarageSalary: {
+      type: "integer",
       int: true
+    },
+    totalRating: {
+      type: "integer",
+      int: true,
+      min: 0,
+      max: 5
     },
     materials: {
       collection: "material",
       via: "minedIn"
     }
+  },
+  beforeValidate: function(values, cb) {
+    // TODO: Calculate the total rating
+    values.totalRating = 1;
+    cb();
   }
 };
 
--- api/models/Material.js
+++ api/models/Material.js
@@ -5,45 +5,73 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
-var RATINGS = [1, 2, 3, 4, 5];
-
 module.exports = {
   descriptions: {
     name: "The name of the material",
-    ratingEnv: "The environment-rating of this material",
-    ratingSoc: "The social-rating of this material",
-    ratingHum: "The human-rating of this material",
+    recyclingRating: "The environment-rating of this material",
+    healthRating: "The health-risks related to this material",
+    countryRating: "The rating of working and political conditions in the country where this material is mined",
     minedIn: "The land where the material originally come from"
   },
   attributes: {
-  	name: {
-  		type: "string",
-  		notEmpty: true,
-  		unique: true,
-  		required: true
+    name: {
+      type: "string",
+      notEmpty: true,
+      unique: true,
+      required: true
     },
-    ratingEnv: {
-      type: "integer",
-      enum: RATINGS,
-      int: true
+    description: {
+      type: "string"
     },
-    ratingSoc: {
+    recyclingRating: {
       type: "integer",
-      enum: RATINGS,
-      int: true
+      int: true,
+      min: 1,
+      max: 5
     },
-    ratingHum: {
+    healthRating: {
       type: "integer",
-      enum: RATINGS,
-      int: true
+      int: true,
+      min: 0,
+      max: 4
+    },
+    countryRating: {
+      type: "int",
+      int: true,
+      min: 1,
+      max: 5
     },
     minedIn: {
       collection: "land",
       via: "materials"
     },
-    inPart: {
+    usedIn: {
       model: "part"
+    },
+  },
+  beforeCreate: function(values, cb) {
+    
+    if (typeof values.minedIn === "Array") {
+      var sum = 0;
+      values.minedIn.forEach(function(countryID) {
+        Land.find({id: countryID}).exec(function(err, response) {
+          if (err) sails.log(err);
+
+          sum += response.totalRating
+        });
+      });
+
+      values.countryRating = sum / values.minedIn.length;
+      cb();
+    } else {
+      Land.find({id: values.minedIn}).exec(function(err, response) {
+        if (err) sails.log(err);
+
+        values.countryRating = response[0].totalRating;
+        cb();
+      }); 
     }
+
   }
 };
 
